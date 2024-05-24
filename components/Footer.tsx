8e3c764ac0d584d9f779bc1c362aad4c513f8e18
Bartosz  Podemski <bartekpodemski@gmail.com>
2024-05-24 21:16:28 +0200

    Small bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,8 +5,8 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10 " id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+    <footer className="w-full mb-[100px] md:mb-5 pb-10 " id="contact">
+      <div className="w-full absolute left-0 -bottom-72 min-h-96 pointer-events-none">
         <img
           src="/footer-grid.svg"
           alt="grid"
